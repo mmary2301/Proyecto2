6de14bc3e24d2e4d7397b552939f5902e3193f6d
mmary2301 <104003044+mmary2301@users.noreply.github.com>
2022-05-21 17:20:39 -0500

    Delete BichoAlien.java

--- BichoAlien.java
+++
@@ -1,8 +0,0 @@
-public class BichoAlien extends Bicho {
-    public BichoAlien(String nombre,int salud){
-        super(nombre,salud);
-    }
-    public BichoAlien(){
-        this("BA-",20);
-    }
-}
